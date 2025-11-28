a5eea6460787da09b6c53cc6ef67bc3892170f4a
Jiri Cerny <jiri.cerny@ibt.cas.cz>
2025-11-28 10:53:41 +0100

    Indentation in SwitchBox

--- src/apps/rednatco/SwitchBox.tsx
+++ src/apps/rednatco/SwitchBox.tsx
@@ -44,7 +44,7 @@ export default function SwitchBox({
           {options.map((option) => (
             <button
               key={option.name}
-              className={`w-full text-left flex my-1 p-1 rounded ${
+              className={`w-full text-left flex my-1 p-1 rounded ml-2 ${
                 activeOption === option.name ? "font-roboto-bold" : "font-roboto-regular"
               }`}
               onClick={() => handleSelect(option)}
